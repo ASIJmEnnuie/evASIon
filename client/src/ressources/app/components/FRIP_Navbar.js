875dbbcbfd9985cf68ebfb733c158f0e02b572f1
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2016-10-23 14:48:43 +0200

    creation de la navbar online

---
+++ client/src/ressources/app/components/FRIP_Navbar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import ActionReorder from 'material-ui/svg-icons/action/reorder';
+
+var FRIP_IconLeft = React.createClass({
+  render: function() {
+    return(
+      <IconButton onTouchTap={this.props.openLeftDrawer}>
+        <ActionReorder />
+      </IconButton>
+    );
+  }
+});
+
+
+
+var FRIP_NavbarOnline = React.createClass({
+  getDefaultProps: function() {
+    return {
+      title:"evASIon",
+    }
+  },
+
+  render: function() {
+    return (
+      <AppBar
+        title={this.props.title}
+        iconElementLeft={<FRIP_IconLeft openLeftDrawer={this.props.clickOnLeftButton} />}
+      />
+    );
+  }
+});
+
+export default FRIP_NavbarOnline;
