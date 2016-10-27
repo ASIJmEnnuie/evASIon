d462d785a07f4e390c42695d89ab48ab0bc586d1
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2016-10-27 14:55:36 +0200

    Test en utilisant simplement slick -> peu concluant

--- client/src/ressources/app/containers/online_pages/FRIP_HomePage.js
+++ client/src/ressources/app/containers/online_pages/FRIP_HomePage.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
 const FRIP_HomePage = React.createClass({
+  componentDidMount: function() {
+    $("#yolo").slick();
+  },
+
   render: function() {
     return (
       <div id={this.props.id}>
         <h1>Bienvenue, vous êtes actuellement sur la page HomeOnline</h1>
+        <div id="yolo" data-slick='{"slidesToShow": 3, "slidesToScroll": 1, "mobileFirst": true, "arrows": false, "centerMode": true}'>
+          <div><h3>1</h3></div>
+          <div><h3>2</h3></div>
+          <div><h3>3</h3></div>
+          <div><h3>4</h3></div>
+          <div><h3>5</h3></div>
+          <div><h3>6</h3></div>
+        </div>
       </div>
     );
   }
