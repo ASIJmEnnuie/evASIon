dd22f12de97202b1589df00b6856d1fbabfb5c81
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2016-10-27 16:25:35 +0200

    Carousel opérationnel

---
+++ client/src/ressources/app/components/FRIP_Carousel.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Carousel from 'nuka-carousel';
+
+const FRIP_Carousel = React.createClass({
+  getInitialState: function() {
+    return {
+      animationDuration: 1000,
+      autoplay: true,
+      autoplayInterval: 3000,
+      infiniteLoop: true,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+    }
+  },
+
+  render: function() {
+    return (
+      <Carousel
+        animationDuration={this.state.animationDuration}
+        autoplay={this.state.autoplay}
+        autoplayInterval={this.state.autoplayInterval}
+        slidesToShow={this.state.slidesToShow}
+        slidesToScroll={this.state.slidesToScroll}
+        wrapAround={this.state.infiniteLoop}
+      >
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+        <div>7</div>
+        <div>8</div>
+        <div>9</div>
+      </Carousel>
+    );
+  }
+});
+
+export default FRIP_Carousel;
--- client/src/ressources/app/containers/online_pages/FRIP_HomePage.js
+++ client/src/ressources/app/containers/online_pages/FRIP_HomePage.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import FRIP_Carousel from '../../components/FRIP_Carousel';
 
 const FRIP_HomePage = React.createClass({
   render: function() {
     return (
       <div id={this.props.id}>
-        <h1>Bienvenue, vous êtes actuellement sur la page HomeOnline</h1>
+        <div id="homeOnlinePage">
+          <h1>Bienvenue, vous êtes actuellement sur la page HomeOnline</h1>
+          <FRIP_Carousel/>
+        </div>
       </div>
     );
   }
