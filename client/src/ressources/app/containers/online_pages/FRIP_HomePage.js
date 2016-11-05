79ff26a7cd3bb9d998b34b691fa73d86ad112f0f
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2016-11-05 14:03:40 +0100

    Changement de l'apparence des slides du carousel

--- client/src/ressources/app/containers/online_pages/FRIP_HomePage.js
+++ client/src/ressources/app/containers/online_pages/FRIP_HomePage.js
@@ -1,34 +1,59 @@
 import React from 'react';
 import {FRIP_Carousel} from '../../components/FRIP_Carousel';
 
+const colors = require('../../../data/colors.json').colors;
+
 const FRIP_HomePage = React.createClass({
   getDefaultProps: function() {
     return {
-      eventList: [1,2,3,4,5,6],
+      eventList: ["Une sortie au musée des beaux arts?", "Un cours de cuisine?", "Une sortie cinéma?"],
+      categorieList: ["Sport", "Concerts", "Cinéma", "Visites"]
     }
   },
 
   render: function() {
-    var slides = (<div className="slideEvent"></div>);
+    var eventSlides = (<div className="slide-event"></div>);
+    var categorieSlides = (<div className="slide-categorie"></div>);
 
-    if (this.props.eventList.length > 0)
-      slides = this.props.eventList.map(function(event, i){
+    if (this.props.eventList.length > 0) {
+      eventSlides = this.props.eventList.map(function(event, i){
         return (
-          <div key={"slideEvent"+i} className="slideEvent"> {event} </div>
+          <div
+            key={"slide-event"+i}
+            className="slide-event"
+            style={{backgroundColor: colors[parseInt(Math.random()*colors.length)]}}
+          >
+            <div> {event} </div>
+          </div>
         );
       });
+    }
+
+    if (this.props.categorieList.length > 0) {
+      categorieSlides = this.props.categorieList.map(function(categorie, i){
+        return (
+          <div
+            key={"slide-categorie"+i}
+            className="slide-categorie"
+            style={{backgroundColor: colors[parseInt(Math.random()*colors.length)]}}
+          >
+            <div> {categorie} </div>
+          </div>
+        );
+      });
+    }
 
     return (
       <div id={this.props.id}>
         <div id="homeOnlinePage">
           <FRIP_Carousel>
-            {slides}
+            {eventSlides}
           </FRIP_Carousel>
           <div id="homeOnlineText">
             <h1>Autres Evenements</h1>
           </div>
           <FRIP_Carousel>
-            {slides}
+            {categorieSlides}
           </FRIP_Carousel>
         </div>
       </div>
