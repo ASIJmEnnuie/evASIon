89a5dd426067d3f032aa7451d69a06e8584bc6b6
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2016-11-07 17:09:08 +0100

    Ajout d'items dans la barre de recherche des evenements

--- client/src/ressources/app/components/FRIP_SearchController.js
+++ client/src/ressources/app/components/FRIP_SearchController.js
@@ -1,22 +1,93 @@
 import React from 'react';
 import TextField from 'material-ui/TextField';
 import DatePicker from 'material-ui/DatePicker';
+import TimePicker from 'material-ui/TimePicker';
+import Slider from 'material-ui/Slider';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+
 import RaisedButton from 'material-ui/RaisedButton';
 
 const FRIP_SearchController = React.createClass({
+  getInitialState: function(){
+    return {
+      value: 1,
+    }
+  },
+
+  handleChange: function (event, index, value) {
+    this.setState({value});
+  },
+
   render: function() {
     return (
       <div className="searchController">
         <div className="searchSelector">
+          <TextField
+            hintText="Nom"
+            onChange={this.props.nameController}
+          />
+        </div>
+
+        <div className="searchSelector">
+          <TextField
+            hintText="Lieu"
+            onChange={this.props.placeController}
+          />
+        </div>
+
+        <div className="searchSelector">
           <DatePicker
-            hintText="Choisir une date"
-            onChange={this.props.dateController}/>
+            hintText="Date"
+            onChange={this.props.dateController}
+          />
         </div>
-        <div className="searchSelector" onChange={this.props.placeController}>
-          <TextField  hintText="Saisir un emplacement" />
+
+        <div className="searchSelector">
+          <TimePicker
+            hintText="Heure début"
+            onChange={this.props.timeController}
+          />
         </div>
-        <div className="searchSelector" onTouchTap={this.props.razController}>
-          <RaisedButton label="Afficher tous les évènements" />
+
+        <div className="searchSelector">
+          <SelectField
+            floatingLabelText="Catégorie"
+            value={this.state.value}
+            onChange={this.handleChange}
+            className="select"
+          >
+            <MenuItem value={1} primaryText="Never" />
+            <MenuItem value={2} primaryText="Every Night" />
+            <MenuItem value={3} primaryText="Weeknights" />
+            <MenuItem value={4} primaryText="Weekends" />
+            <MenuItem value={5} primaryText="Weekly" />
+          </SelectField>
+        </div>
+
+        <div className="searchSelector">
+          <SelectField
+            floatingLabelText="Tags"
+            value={this.state.value}
+            onChange={this.handleChange}
+            className="select"
+          >
+            <MenuItem value={1} primaryText="Never" />
+            <MenuItem value={2} primaryText="Every Night" />
+            <MenuItem value={3} primaryText="Weeknights" />
+            <MenuItem value={4} primaryText="Weekends" />
+            <MenuItem value={5} primaryText="Weekly" />
+          </SelectField>
+        </div>
+
+        <div className="searchSelector">
+          <div className="text">Prix</div>
+          <Slider className="slider"/>
+        </div>
+
+        <div className="searchSelector">
+          <div className="text">Proximité</div>
+          <Slider className="slider"/>
         </div>
       </div>
     )
