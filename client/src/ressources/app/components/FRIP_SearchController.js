ec482eb42b50a2a4ea85e0e03569f1c067104f01
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2016-11-08 10:11:34 +0100

    Modification du style de la barre de recherche des events ->
    une seule ligne  de selectors + alignement des selectors + placement relatif du bouton d'extension

--- client/src/ressources/app/components/FRIP_SearchController.js
+++ client/src/ressources/app/components/FRIP_SearchController.js
@@ -26,78 +26,82 @@ const FRIP_SearchController = React.createClass({
   },
 
   deploySearchBar: function() {
-    
+
   },
 
   render: function() {
     return (
       <div className="searchController">
-        <div className="searchSelector">
-          <TextField
-            hintText="Nom"
-            onChange={this.props.nameController}
-          />
+        <div className="princalControl" >
+          <div className="searchSelector">
+            <TextField
+              hintText="Nom"
+              onChange={this.props.nameController}
+            />
+          </div>
+
+          <div className="searchSelector">
+            <DatePicker
+              hintText="Date"
+              onChange={this.props.dateController}
+            />
+          </div>
+
+          <div className="searchSelector">
+            <SelectField
+              floatingLabelText="Catégorie"
+              value={this.state.value}
+              onChange={this.handleChange}
+              className="select"
+            >
+              <MenuItem value={1} primaryText="Never" />
+              <MenuItem value={2} primaryText="Every Night" />
+              <MenuItem value={3} primaryText="Weeknights" />
+              <MenuItem value={4} primaryText="Weekends" />
+              <MenuItem value={5} primaryText="Weekly" />
+            </SelectField>
+          </div>
+
+          <div className="searchSelector">
+            <div className="text">Prix</div>
+            <Slider className="slider"/>
+          </div>
         </div>
 
-        <div className="searchSelector">
-          <DatePicker
-            hintText="Date"
-            onChange={this.props.dateController}
-          />
-        </div>
+        <div className="secondaryController">
+          <div className="searchSelector">
+            <TextField
+              hintText="Lieu"
+              onChange={this.props.placeController}
+            />
+          </div>
 
-        <div className="searchSelector">
-          <SelectField
-            floatingLabelText="Catégorie"
-            value={this.state.value}
-            onChange={this.handleChange}
-            className="select"
-          >
-            <MenuItem value={1} primaryText="Never" />
-            <MenuItem value={2} primaryText="Every Night" />
-            <MenuItem value={3} primaryText="Weeknights" />
-            <MenuItem value={4} primaryText="Weekends" />
-            <MenuItem value={5} primaryText="Weekly" />
-          </SelectField>
-        </div>
+          <div className="searchSelector">
+            <TimePicker
+              hintText="Heure début"
+              onChange={this.props.timeController}
+            />
+          </div>
 
-        <div className="searchSelector">
-          <div className="text">Prix</div>
-          <Slider className="slider"/>
-        </div>
+          <div className="searchSelector">
+            <SelectField
+              floatingLabelText="Tags"
+              value={this.state.value}
+              onChange={this.handleChange}
+              className="select"
+            >
+              <MenuItem value={1} primaryText="Never" />
+              <MenuItem value={2} primaryText="Every Night" />
+              <MenuItem value={3} primaryText="Weeknights" />
+              <MenuItem value={4} primaryText="Weekends" />
+              <MenuItem value={5} primaryText="Weekly" />
+            </SelectField>
+          </div>
 
-        <div className="searchSelector">
-          <TextField
-            hintText="Lieu"
-            onChange={this.props.placeController}
-          />
-        </div>
-
-        <div className="searchSelector">
-          <TimePicker
-            hintText="Heure début"
-            onChange={this.props.timeController}
-          />
-        </div>
-
-        <div className="searchSelector">
-          <SelectField
-            floatingLabelText="Tags"
-            value={this.state.value}
-            onChange={this.handleChange}
-            className="select"
-          >
-            <MenuItem value={1} primaryText="Never" />
-            <MenuItem value={2} primaryText="Every Night" />
-            <MenuItem value={3} primaryText="Weeknights" />
-            <MenuItem value={4} primaryText="Weekends" />
-            <MenuItem value={5} primaryText="Weekly" />
-          </SelectField>
-        </div>
-
-        <div className="searchSelector">
-          <div className="text">Proximité</div>
-          <Slider className="slider"/>
+          <div className="searchSelector">
+            <div className="text">Proximité</div>
+            <Slider className="slider"/>
+          </div>
         </div>
 
         <IconButton
