3fa97b819eac14deb39cc55c4427e66d2be0ce45
Legros Morgane <morgane.legros@insa-rouen.fr>
2016-11-10 15:39:04 +0100

    Formulaire Inscription avec conditions

--- client/src/ressources/app/components/FRIP_Form.js
+++ client/src/ressources/app/components/FRIP_Form.js
@@ -25,7 +25,7 @@ var display = function (id) {
 };
 
 var errorDisplay = function(state, id) {
-  if (state.length > 1)
+  if (state.length > 0)
     notDisplay(id);
   else
     display(id);
@@ -52,12 +52,12 @@ var FRIP_FormConnexion = React.createClass({
 
   setEmail: function(event) {
     this.setState({email: event.target.value});
-    errorDisplay(this.state.email, "emailError");
+    errorDisplay(event.target.value, "emailError");
   },
 
   setPassword: function(event) {
     this.setState({password: event.target.value});
-    errorDisplay(this.state.password, "passwordError");
+    errorDisplay(event.target.value, "passwordError");
   },
 
   render: function() {
@@ -73,9 +73,7 @@ var FRIP_FormConnexion = React.createClass({
               onChange={this.setEmail}
             />
           </div>
-          <div>
-            <ErrorText id="emailError" text={this.props.text.errorText} />
-          </div>
+          <ErrorText id="emailError" text={this.props.text.errorText} />
           <div>
             <TextField
               id="password"
@@ -84,13 +82,9 @@ var FRIP_FormConnexion = React.createClass({
               className="form-text"
               onChange={this.setPassword}
             />
-        </div>
-        <div>
+          </div>
           <ErrorText id="passwordError" text={this.props.text.errorText} />
-        </div>
-        <div>
           <RaisedButton type="submit" value="Submit" className="form-button" label={this.props.label} primary={true}/>
-        </div>
         </form>
       </div>
     );
@@ -98,40 +92,127 @@ var FRIP_FormConnexion = React.createClass({
 });
 
 var FRIP_FormInscription = React.createClass({
+  getInitialState: function() {
+    return {
+      familyName: "",
+      firstname: "",
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+    }
+  },
+
+  handleSubmit: function() {
+    if (!this.state.familyName.trim() || !this.state.firstname.trim() || !this.state.email.trim() || !this.state.password.trim() || !this.state.passwordConfirmation.trim()) {
+      return ; // mettre action pour rester sur cette page
+    }
+    // envoie à la BD pour control
+    // si control valide, alors :
+    this.props.connexion();
+    console.log("familyName : "+this.state.familyName);
+    console.log("firstname : "+this.state.firstname);
+    console.log("email : "+this.state.email);
+    console.log("password : "+this.state.password);
+    console.log("passwordConfirmation : "+this.state.passwordConfirmation);
+  },
+
+  setFamilyName: function(event) {
+    this.setState({familyName: event.target.value});
+    errorDisplay(event.target.value, "familyNameError");
+  },
+
+  setFirstname: function(event) {
+    this.setState({firstname: event.target.value});
+    errorDisplay(event.target.value, "firstnameError");
+  },
+
+  setEmail: function(event) {
+    this.setState({email: event.target.value});
+    errorDisplay(event.target.value, "emailError");
+  },
+
+  setPassword: function(event) {
+    this.setState({password: event.target.value});
+    errorDisplay(event.target.value, "passwordError");
+    this.errorPassword("password", event.target.value, "passwordConfirmationError");
+  },
+
+  setPasswordConfirmation: function(event) {
+    this.setState({passwordConfirmation: event.target.value});
+    this.errorPassword("passwordConfirmation", event.target.value, "passwordConfirmationError");
+  },
+
+  errorPassword: function(name, value, idError) {
+    if (name == "password") {
+      if (value == this.state.passwordConfirmation)
+        notDisplay(idError);
+      else
+        display(idError);
+    }
+    else {
+      if (name == "passwordConfirmation") {
+        if (value == this.state.password)
+          notDisplay(idError);
+        else
+          display(idError);
+      }
+    }
+  },
 
   render: function() {
     return (
       <div>
-        <h2 className="form-title">{this.props.text.nameFormInscription}</h2>
-        <TextField
-          id="name"
-          placeholder={this.props.text.name}
-          className="form-text"
-        /><br />
-        <TextField
-          id="surname"
-          placeholder={this.props.text.surname}
-          className="form-text"
-        /><br />
-        <TextField
-          id="email"
-          type="email"
-          placeholder={this.props.text.email}
-          className="form-text"
-        /><br />
-        <TextField
-          id="password"
-          type="password"
-          placeholder={this.props.text.password}
-          className="form-text"
-        /><br />
-        <TextField
-          id="passwordConfirmation"
-          type="password"
-          placeholder={this.props.text.passwordConfirmation}
-          className="form-text"
-        /><br />
-        <RaisedButton className="form-button" label={this.props.label} primary={true} onTouchTap={this.props.connexion}/>
+        <form onSubmit={this.handleSubmit}>
+          <h2 className="form-title">{this.props.text.nameFormInscription}</h2>
+          <div>
+            <TextField
+              id="familyName"
+              placeholder={this.props.text.familyName}
+              className="form-text"
+              onChange={this.setFamilyName}
+            />
+          </div>
+          <ErrorText id="familyNameError" text={this.props.text.errorText} />
+          <div>
+            <TextField
+              id="firstname"
+              placeholder={this.props.text.firstname}
+              className="form-text"
+              onChange={this.setFirstname}
+            />
+          </div>
+          <ErrorText id="firstnameError" text={this.props.text.errorText} />
+          <div>
+            <TextField
+              id="email"
+              placeholder={this.props.text.email}
+              className="form-text"
+              onChange={this.setEmail}
+            />
+          </div>
+          <ErrorText id="emailError" text={this.props.text.errorText} />
+          <div>
+            <TextField
+              id="password"
+              type="password"
+              placeholder={this.props.text.password}
+              className="form-text"
+              onChange={this.setPassword}
+            />
+          </div>
+          <ErrorText id="passwordError" text={this.props.text.errorText} />
+          <div>
+            <TextField
+              id="passwordConfirmation"
+              type="password"
+              placeholder={this.props.text.passwordConfirmation}
+              className="form-text"
+              onChange={this.setPasswordConfirmation}
+            />
+          </div>
+          <ErrorText id="passwordConfirmationError" text={this.props.text.errorTextDifferentPassword} />
+          <RaisedButton type="submit" className="form-button" label={this.props.label} primary={true}/>
+        </form>
       </div>
     );
   },
