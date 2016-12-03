213374ef7c6f59de86af95829a0155f991be68da
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2016-12-03 12:12:10 +0100

    Modifications stylistiques mineures page d'inscription

--- client/src/ressources/app/containers/offline_pages/FRIP_InscriptionPage.js
+++ client/src/ressources/app/containers/offline_pages/FRIP_InscriptionPage.js
@@ -2,17 +2,24 @@ import React from 'react';
 
 import {FRIP_FormInscription} from '../../components/FRIP_Form';
 
-var FRIP_InscriptionPage = React.createClass({
+const FRIP_InscriptionPage = React.createClass({
 
   render: function() {
     return (
-      <div className="form" id={this.props.id}>
+      <div
+        className="form"
+        id={this.props.id}
+      >
         <FRIP_FormInscription
           data={this.props.data.form}
           label={this.props.data.buttonInscriptionLabel}
           connexion={this.props.connexion}
         />
-        <div className="link" onTouchTap={this.props.changePage}>{this.props.data.cancelLabel}</div>
+        <div
+          className="link"
+          onTouchTap={this.props.changePage}>
+            {this.props.data.cancelLabel}
+        </div>
       </div>
     );
   }
