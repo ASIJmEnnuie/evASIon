635f1c797c2a90550beec66f6a49762350de764a
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2016-12-03 10:44:13 +0100

    Correction des couleurs depuis l'index

--- client/src/ressources/app/index.jsx
+++ client/src/ressources/app/index.jsx
@@ -4,7 +4,7 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import {fade} from 'material-ui/utils/colorManipulator';
-import { indigo400, indigo700, pinkA200, grey100, grey300, grey400, grey500, white, darkBlack, fullBlack } from 'material-ui/styles/colors';
+import {grey100, grey300, grey400, grey500, white, darkBlack, fullBlack } from 'material-ui/styles/colors';
 
 var parameters = require('../data/parameters.json');
 
@@ -13,8 +13,8 @@ import FRIP_Global from './FRIP_Global';
 const muiTheme = getMuiTheme({
   fontFamily: 'Roboto, sans-serif',
   palette: {
-    primary1Color: parameters.secondaryColor2,
-    primary2Color: parameters.secondaryColor,
+    primary1Color: parameters.primaryColor,
+    primary2Color: parameters.primaryColor,
     primary3Color: grey400,
     accent1Color: parameters.primaryColor,
     accent2Color: grey100,
@@ -25,7 +25,7 @@ const muiTheme = getMuiTheme({
     canvasColor: white,
     borderColor: grey300,
     disabledColor: fade(darkBlack, 0.3),
-    pickerHeaderColor: indigo400,
+    pickerHeaderColor: parameters.primaryColor,
     clockCircleColor: fade(darkBlack, 0.07),
     shadowColor: fullBlack,
   }
