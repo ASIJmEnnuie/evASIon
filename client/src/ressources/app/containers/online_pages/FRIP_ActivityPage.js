d769ec4d485de394a384ae21c2fa3bc76af59dc9
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2016-12-05 09:59:53 +0100

    Ajout de la page Activity

---
+++ client/src/ressources/app/containers/online_pages/FRIP_ActivityPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FRIP_ActivityPage = React.createClass({
+  render: function() {
+    return (
+      <div id={this.props.id}>
+        ActivityPage
+      </div>
+    )
+  }
+});
+
+export default FRIP_ActivityPage;
